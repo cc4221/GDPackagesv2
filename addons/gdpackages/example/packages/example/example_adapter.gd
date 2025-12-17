20d1f58a4624cd164ceaadc5301dd5a6a2b0e468
Elitium <70774230+cc4221@users.noreply.github.com>
2025-12-17 19:39:43 +0300

    Delete addons/gdpackages directory

--- addons/gdpackages/example/packages/example/example_adapter.gd
+++
@@ -1,7 +0,0 @@
-extends PackageAdapter
-
-static func say_hello() -> void:
-    print("Hello, World!")
-
-static func say_goodbye() -> void:
-    print("Goodbye, World!")
--- addons/gdpackages/plugin/package_context_menu_plugin.gd
+++
@@ -1,7 +0,0 @@
-@tool
-extends EditorContextMenuPlugin
-
-signal pressed
-
-func _popup_menu(paths: PackedStringArray) -> void:
-    add_context_menu_item("Package", pressed.emit)
